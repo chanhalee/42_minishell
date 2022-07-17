cec1ce1a7f6fe3afc0f1374f2838474345ad3751
jeounpar <cdex6531@ajou.ac.kr>
2022-07-17 21:01:00 +0900

    add main.c & add char *str to global variable

---
+++ minishell/main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include "./parsing/command_parse.h"
+# include "./parsing/do_not_submit/do_not_submit.h"
+
+void	signal_handler(int signo)
+{
+	if (signo == SIGINT)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 1);
+		rl_redisplay();
+	}
+}
+
+void	print_intro(void)
+{
+	int		intro_fd;
+    int		readsize;
+	char	buffer[43];
+
+	intro_fd = open("intro.txt", O_RDONLY);
+    if (intro_fd < 0)
+	{
+		printf("fail to open intro.txt\n");
+        return ;
+	}
+    while (1)
+    {
+        readsize = read(intro_fd, buffer, 42);
+		buffer[42] = '\0';
+        if (readsize < 1) {
+            printf("\n");
+            break;
+        }
+        printf("%s", buffer);
+    }
+    close(intro_fd);
+}
+
+void	init_env_and_signal(char **env)
+{
+	struct termios term;
+
+	ft_initlist(&(g_state.list));
+	init_env(env, &(g_state.head), &(g_state.list));
+
+	signal(SIGINT, signal_handler);
+	signal(SIGQUIT, SIG_IGN);
+
+	// ctrl + C 입력시 반향을 꺼줌
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~(ECHOCTL);
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+void	prompt(t_cmd_list *cmd_lst)
+{
+    while(1)
+    {
+        g_state.str = readline("bash$ ");
+        if (g_state.str)
+		{
+			if (g_state.str[0] != 0)
+			{
+				cmd_lst = parse(ft_p_strdup(g_state.str));
+				print_cmd_lists(cmd_lst);
+				free_t_cmd_list(cmd_lst);
+        		add_history(g_state.str);
+			}
+		}
+        else
+		{
+			printf("\033[1A");
+            printf("\033[5C");
+            printf(" exit\n");
+            break ;
+		}
+        free(g_state.str);
+		g_state.str = NULL;
+    }
+}
+
+int main(int argc, char **argv, char **env)
+{
+	t_cmd_list	*cmd_lst;
+
+	print_intro();
+	init_env_and_signal(env);
+	prompt(cmd_lst);
+
+	system("leaks minishell");
+    return(0);
+}
