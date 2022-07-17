4231f70fd6efd44249f99492c375ffab2951a611
chanhale <chan771788@gmail.com>
2022-07-17 13:41:15 +0900

    func naming collision with exec part -handled, parsing exec_file_name added

--- minishell/parsing/do_not_submit/for_exe_part.c
+++ minishell/parsing/do_not_submit/for_exe_part.c
@@ -6,7 +6,7 @@
 /*   By: chanhale <chanhale@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 12:25:05 by chanhale          #+#    #+#             */
-/*   Updated: 2022/07/17 13:12:50 by chanhale         ###   ########.fr       */
+/*   Updated: 2022/07/17 13:35:55 by chanhale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 // 실행 전 syntax error 에 대한 식별이 필요함 cmd_list->status == TYPE_SYNTAX_ERR 라면 실행하면 안됨.
 // 실행 전 cmd->string != NULL 에 대한 처리가 필요함 (거짓일 경우 interprete_exe_name를 실행시켜선 안됨)
+// 실행 전 builtin 함수인지에 대한 확인이 필요하다. (builtin이 참일경우 실행하면 안된다.)
+// 위 경우 fork 조차 시켜선 안될듯?
+
+// 하기 interprete_exe_name 함수는 fork 이후에 실행되어야 한다.
 void interprete_exe_name(t_cmd *cmd)
 {
 	char	*str;
@@ -36,4 +40,24 @@ void interprete_exe_name(t_cmd *cmd)
 		free (str);
 	}
 	parse_safe_free_two_d_char(sep, -1);
+}
+
+// 실행 전 syntax error 에 대한 식별이 필요함 cmd_list->status == TYPE_SYNTAX_ERR 라면 실행하면 안됨.
+// 실행 전 cmd->string != NULL 에 대한 처리가 필요함 (거짓일 경우 check_exec_name_is_builtin를 실행시켜선 안됨)
+void check_exec_name_is_builtin(t_cmd *cmd)
+{
+	if (ft_p_strcmp(cmd->exec_file_name, "echo"))
+		;//
+	else if (ft_p_strcmp(cmd->exec_file_name, "cd"))
+		;//
+	else if (ft_p_strcmp(cmd->exec_file_name, "pwd"))
+		;//
+	else if (ft_p_strcmp(cmd->exec_file_name, "export"))
+		;//
+	else if (ft_p_strcmp(cmd->exec_file_name, "unset"))
+		;//
+	else if (ft_p_strcmp(cmd->exec_file_name, "env"))
+		;//
+	else if (ft_p_strcmp(cmd->exec_file_name, "exit"))
+		;//
 }
