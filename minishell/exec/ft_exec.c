39b808ead208217c38fffb70f4715828a8ac05e2
jeounpar <cdex6531@ajou.ac.kr>
2022-07-20 01:26:50 +0900

    is pipe done...?

--- minishell/exec/ft_exec.c
+++ minishell/exec/ft_exec.c
@@ -12,36 +12,96 @@ void	fork_fail(void)
 	exit(TYPE_FAIL);
 }
 
+void	set_pipe(t_cmd *cmd)
+{
+	if (cmd->type == TYPE_PIPE
+		|| (cmd->next != 0 && cmd->next->type == TYPE_PIPE))
+	{
+		if (cmd->type != TYPE_PIPE)
+			dup2(cmd->fds[1], 1);
+		else if (cmd->next == 0)
+			dup2(cmd->prev->fds[0], 0);
+		else
+		{
+			dup2(cmd->prev->fds[0], 0);
+			dup2(cmd->fds[1], 1);
+		}
+	}
+}
+// void	set_pipe(t_cmd *cmd)
+// {
+// 	if (cmd->next != 0)
+// 	{
+// 		dup2(cmd->fds[1], 1);
+// 		if (cmd->next == 0)
+// 			dup2(cmd->prev->fds[0], 0);
+// 		else
+// 		{
+// 			dup2(cmd->prev->fds[0], 0);
+// 			dup2(cmd->fds[1], 1);
+// 		}
+// 	}
+// }
+
+void	close_fd_dup(t_cmd *cmd, int *stin, int *stout)
+{
+	if (cmd->prev != 0)
+		close(cmd->prev->fds[0]);
+	close(cmd->fds[1]);
+	if (cmd->next == 0)
+		close(cmd->fds[0]);
+	if (cmd->fd_in != 0)
+		close(cmd->fd_in);
+	if (cmd->fd_out != 1)
+		close(cmd->fd_out);
+	dup2(*stin, 0);
+	dup2(*stout, 1);
+	close(*stin);
+	close(*stout);
+}
+
 void	pipes(t_cmd *cmd)
 {
 	int	pid;
 	int	ret;
 	int	status;
-	int	pipes[2];
 
 	g_state.is_fork = 1;
-	if (pipe(pipes))
-		exit(1);
 	pid = fork();
 	if (pid < 0)
-		fork_fail();
-	else if (pid > 0)
-	{
-		// close(pipes[0]);
-		dup2(pipes[1], 1);
-		close(pipes[1]);
-		ret = wait(&status);
-		g_state.exit_code = WEXITSTATUS(status);
-	}
+		exit(1);
 	else if (pid == 0)
 	{
-		dup2(pipes[0], 0);
-		// close(pipes[0]);
-		close(pipes[1]);
+		set_pipe(cmd);
+		if (cmd->fd_in != 0)
+			dup2(cmd->fd_in, 0);
+		if (cmd->fd_out != 1)
+			dup2(cmd->fd_out, 1);
 		ret = exec_builtin(cmd);
 		if (ret == -1)
 			ret = execve(cmd->argv[0], cmd->argv, NULL);
 		exit(ret);
+	}
+	else
+	{
+		// close(pipes[0]);
+		close(cmd->fds[1]);
+		wait(&status);
+		g_state.exit_code = WEXITSTATUS(status);
+	}
+}
+
+void	set_type(t_cmd_list *lists)
+{
+	t_cmd	*cmd;
+
+	cmd = lists->cmd_list;
+	cmd->type = TYPE_NORMAL;
+	cmd = cmd->next;
+	while(cmd != NULL)
+	{
+		cmd->type = TYPE_PIPE;
+		cmd = cmd->next;
 	}
 }
 
@@ -51,12 +111,17 @@ int	ft_exec(t_cmd_list *lists)
 	int		pipe_open;
 	int		path_exec;
 	int		builtin_exec;
+	int		std_in;
+	int		std_out;
 
+	set_type(lists);
+	
 	cmd = lists->cmd_list;
 	while (cmd != NULL)
 	{
-		g_state.std_out = dup(STDOUT);
-		g_state.std_in = dup(STDIN);
+		std_out = dup(STDOUT);
+		std_in = dup(STDIN);	
+		pipe(cmd->fds);
 		builtin_exec = check_exec_name_is_builtin(cmd);
 		path_exec = interprete_exe_name(cmd);
 		if (builtin_exec == 0 || path_exec == 0)
@@ -64,8 +129,10 @@ int	ft_exec(t_cmd_list *lists)
 			ft_redirection(cmd);
 			// check_exec_name_is_builtin(cmd);
 			pipes(cmd);
+			close_fd_dup(cmd, &std_in, &std_out);
 			if (g_state.exit_code == 34 && cmd->prev == NULL)
 				return (34);
+			g_state.is_fork = 0;
 		}
 		else
 		{
@@ -75,9 +142,8 @@ int	ft_exec(t_cmd_list *lists)
 				printf("bash: %s: No such file or directory\n", cmd->argv[0]);
 			g_state.exit_code = 127;
 		}
-		dup2(g_state.std_in, STDIN);
-		dup2(g_state.std_out, STDOUT);
-		g_state.is_fork = 0;
+		// dup2(g_state.std_in, STDIN);
+		// dup2(g_state.std_out, STDOUT);
 		cmd = cmd->next;
 	}
 	return (0);
--- minishell/exec/redirection.c
+++ minishell/exec/redirection.c
@@ -6,7 +6,7 @@
 /*   By: jeounpar <jeounpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 20:07:22 by jeounpar          #+#    #+#             */
-/*   Updated: 2022/07/19 21:16:24 by jeounpar         ###   ########.fr       */
+/*   Updated: 2022/07/20 00:47:36 by jeounpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,26 +21,26 @@ void	ft_redirection(t_cmd *cmd)
 	int					output_fd;
 	int					input_fd;
 
-	g_state.std_out = dup(STDOUT);
-	g_state.std_in = dup(STDIN);
+	cmd->fd_in = 0;
+	cmd->fd_out = 1;
 	red = cmd->redirection_list;
 	while (red != NULL)
 	{
 		if (red->red_type == TYPE_TOKEN_IO_R)
-			g_state.fd_out = open(red->file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+			cmd->fd_out = open(red->file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 		else if (red->red_type == TYPE_TOKEN_IO_RR)
-			g_state.fd_out = open(red->file, O_CREAT | O_WRONLY | O_APPEND, 0644);
+			cmd->fd_out = open(red->file, O_CREAT | O_WRONLY | O_APPEND, 0644);
 		else if (red->red_type == TYPE_TOKEN_IO_L)
-			g_state.fd_in = open(red->file, O_RDONLY);
-		if (g_state.fd_out == -1 || g_state.fd_in == -1)
+			cmd->fd_in = open(red->file, O_RDONLY);
+		if (cmd->fd_out == -1 || cmd->fd_in== -1)
 		{
 			printf("bash: %s: No such file or directory\n", red->file);
 			g_state.exit_code = 127;
 		}
-		if (red->red_type == TYPE_TOKEN_IO_R || red->red_type == TYPE_TOKEN_IO_RR)
-			dup2(g_state.fd_out, STDOUT);
-		else if (red->red_type == TYPE_TOKEN_IO_L)
-			dup2(g_state.fd_in, STDIN);
+		// if (red->red_type == TYPE_TOKEN_IO_R || red->red_type == TYPE_TOKEN_IO_RR)
+		// 	dup2(g_state.fd_out, STDOUT);
+		// else if (red->red_type == TYPE_TOKEN_IO_L)
+		// 	dup2(g_state.fd_in, STDIN);
 		red = red->next;
 	}
 }
